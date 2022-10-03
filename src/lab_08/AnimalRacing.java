1db1ec7730a7a71c83731899284450e827d8f856
lqhung2207 <qhung2207@gmail.com>
2022-10-03 17:41:47 +0700

    Lab08

--- src/lab_08/AnimalRacing.java
+++ src/lab_08/AnimalRacing.java
@@ -1,14 +1,11 @@
 package lab_08;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AnimalRacing {
-
-    public static void main(String[] args) {
-        Bear bear = new Bear();
-        Horse horse = new Horse();
-        Panda panda = new Panda();
-
+    public static void racing(Bear bear, Horse horse, Panda panda){
         int bearSpeed = bear.getSpeed();
         int horseSpeed = horse.getSpeed();
         int pandaSpeed = panda.getSpeed();
@@ -17,12 +14,11 @@ public class AnimalRacing {
         System.out.println("Speed of " + horse.getAnimalName() + " is " + horseSpeed);
         System.out.println("Speed of " + panda.getAnimalName() + " is " + pandaSpeed);
 
-        List<Integer> animalList = new ArrayList<>();
-        animalList.add(bearSpeed);
-        animalList.add(horseSpeed);
-        animalList.add(pandaSpeed);
-
-        int max = Collections.max(animalList);
+        List<Integer> animalSpeed = new ArrayList<>();
+        animalSpeed.add(bearSpeed);
+        animalSpeed.add(horseSpeed);
+        animalSpeed.add(pandaSpeed);
+        int max = Collections.max(animalSpeed);
         if (bearSpeed == max) {
             System.out.println("Winner is: " + bear.getAnimalName() + " with speed " + bearSpeed);
         } else if (horseSpeed == max) {
@@ -31,4 +27,12 @@ public class AnimalRacing {
             System.out.println("Winner is: " + panda.getAnimalName() + " with speed " + pandaSpeed);
         }
     }
+
+    public static void main(String[] args) {
+        Bear bear = new Bear();
+        Horse horse = new Horse();
+        Panda panda = new Panda();
+        racing(bear, horse, panda);
+    }
 }
+
